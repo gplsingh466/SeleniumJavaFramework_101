d473a09765c6ac2216797f91778eaa92be95faaf
gplsingh46 <gplsingh466@gmail.com>
2025-12-01 17:10:41 +0530

    Added one more Test case

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -1,7 +1,12 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class FirstSeleniumTest {
 
@@ -10,5 +15,15 @@ public class FirstSeleniumTest {
         WebDriver driver = new ChromeDriver();
         driver.get("https://practicesoftwaretesting.com/");
         Assert.assertEquals("Practice Software Testing - Toolshop - v5.0",driver.getTitle());
+    }
+
+    @Test
+    public void checkContactHeading(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://practicesoftwaretesting.com/contact");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement heading = wait.until(d-> driver.findElement(By.tagName("h3")));
+        String headingText = heading.getText();
+        Assert.assertEquals("Contact",headingText);
     }
 }
