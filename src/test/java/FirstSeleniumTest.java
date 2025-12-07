4962aaec2d66b25a79b2f91f894f01c344027fbf
gplsingh46 <gplsingh466@gmail.com>
2025-12-07 16:51:35 +0530

    Adding- pom.xml changes testng.xml

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -9,17 +10,26 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 public class FirstSeleniumTest {
+    ChromeOptions options = new ChromeOptions();
+
+        WebDriver driver ;
 
     @Test
     public void checkTitle(){
-        WebDriver driver = new ChromeDriver();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
         driver.get("https://practicesoftwaretesting.com/");
         Assert.assertEquals("Practice Software Testing - Toolshop - v5.0",driver.getTitle());
     }
 
     @Test
     public void checkContactHeading(){
-        WebDriver driver = new ChromeDriver();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+       driver = new ChromeDriver(options);
         driver.get("https://practicesoftwaretesting.com/contact");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         WebElement heading = wait.until(d-> driver.findElement(By.tagName("h3")));
